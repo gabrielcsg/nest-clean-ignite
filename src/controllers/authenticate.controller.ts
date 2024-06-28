ac6da660efb877645f0be7e093720d3069f8b22b
Gabriel Castro <gabrielcssg@gmail.com>
2024-06-27 23:56:58 -0300

    feat: jwt auth and create question

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { z } from 'zod';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { ZodValidationPipe } from '../pipes/zod-validation.pipe';
+import { compare } from 'bcryptjs';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AuthenticateBody = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly prismaService: PrismaService
+  ) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() authenticateBody: AuthenticateBody) {
+    const { email, password } = authenticateBody;
+
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User credentials do not match.');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentials do not match.');
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id });
+
+    return { access_token: accessToken };
+  }
+}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,53 @@
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { z } from 'zod';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { ZodValidationPipe } from '../pipes/zod-validation.pipe';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { UserPayload } from '../auth/jwt.strategy';
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(bodyValidationPipe)
+    createQuestionBody: CreateQuestionBody
+  ) {
+    const { title, content } = createQuestionBody;
+    const userId = user.sub;
+    const slug = this.convertToSlug(title);
+
+    await this.prismaService.question.create({
+      data: {
+        title,
+        slug,
+        content,
+        authorId: userId,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-');
+  }
+}
